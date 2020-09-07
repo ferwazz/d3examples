7e0abeb69f1aa6f83938425ec11da26e363e2742
Fernando <fernando.alvarez@uabc.edu.mx>
2020-09-07 09:48:30 -0700

    new features

--- src/donut_chart.js
+++ src/donut_chart.js
@@ -1,8 +1,8 @@
 const data = [
-    {"name": "<5","value": 19912018},
-    {"name": "5-9","value": 20501982},
-    {"name": "10-14","value": 20679786},
-    {"name": "15-19","value": 21354481},
+    {"name": "<5","value": 19912018,"category":1},
+    {"name": "5-9","value": 20501982,"category":1},
+    {"name": "10-14","value": 20679786    },
+    {"name": "15-19","value": 21354481    },
     {"name": "20-24","value": 22604232},
     {"name": "25-29","value": 21698010},
     {"name": "30-34","value": 21183639},
@@ -20,16 +20,16 @@ const data = [
 
 const canvas  = d3.select(".canva");
 
-height = 490
-width = 600
+height = 500
+width = 1000
 
 const svg = canvas.append("svg")
-                .attr("width", width)
-                .attr("height", height);
+                .attr("width", width*1.2)
+                .attr("height", height*1.2);
 
 const margin = {top: 20, bottom: 70, left: 70, right: 70};
-const graphWidth = 700 - margin.left - margin.right;
-const graphHeight = 900 - margin.top - margin.bottom;
+const graphWidth = 1200 - margin.left - margin.right;
+const graphHeight = 1200 - margin.top - margin.bottom;
 
 var formatComma = d3.format(",");
 
@@ -37,20 +37,20 @@ var formatComma = d3.format(",");
 const mainCanvas = svg.append("g")
                         .attr("width", graphWidth / 2)
                         .attr("height", graphHeight / 2)
-                        .attr("transform", `translate(${margin.left + 180}, 
-                            ${margin.top + 220})`);
+                        .attr("transform", `translate(${margin.left + 480}, 
+                            ${margin.top + 300})`);
 
                             
 mainCanvas.append("text")
           .attr("class", "daca-text")
           .attr("dy", "0.2cm")
-          .style("opacity", 0)
           .transition()
                 .duration(1000)
-                .style("opacity", (d,i) => i+0.7)
+                .style("opacity", (d,i) => 0.7-i)
           .text("US Population per age")
           .attr("text-anchor", "middle")
           .attr("font-weight", "bold")
+          .attr("font-size", "25")
 
 
 let n = 17
@@ -59,6 +59,12 @@ colorbyValue = {}
  for (let i = 0; i < n; ++i) {
    colorbyValue[data[i].name] = d3.rgb(d3.interpolateSpectral(i / (n - 1))).hex();
  }
+
+ color = d3.scaleOrdinal()
+        .domain(data.map(d => d.name))
+        .range(d3.quantize(t => d3.interpolateSpectral(t * 0.88 + 0.1), data.length))
+
+
 
 
 var arcs = d3.pie()(data.map(function(d) { return d.value; }));
@@ -69,6 +75,11 @@ const arcpath = d3.arc()
                 .innerRadius(radius * 0.6)
                 .outerRadius(radius - 10)
 
+var outerArc = d3.arc()
+                .innerRadius(radius * 0.9)
+                .outerRadius(radius * 1.25)
+
+
 const paths =  mainCanvas.selectAll("path")
                          .data(arcs)
 
@@ -77,11 +88,28 @@ paths.enter()
     .append("path")
     .attr("class", "arc")
     .attr("stroke", "white")
-    .attr("fill", function(d, i){return colorbyValue[data[i].name]})
+    .attr("vector-effect", "non-scaling-stroke")
+    .style("stroke-width", "2px")
+    .style("opacity", 1)
+    //.attr("fill", function(d, i){return colorbyValue[data[i].name]})
+    .attr("fill", function(d,i) {return color(data[i].name)})
     .attr("d", arcpath)
  
-    
+const labels = mainCanvas.selectAll("label")
+                            .data(arcs)
 
+const verticalGap = 1
 
-
+labels.enter()
+            .append('text')
+            .style("opacity", 1)
+            .text( function(d,i) { console.log(data[i].name) ; return data[i].name })
+            .attr('transform', function(d) {
+                var pos = outerArc.centroid(d)
+                return 'translate(' + pos + ')';
+            })
+            .attr("font-weight", "bold")
+            .attr("font-size", "15")
+            .attr("font-family", "sans-serif")
+            .attr("text-anchor", "middle")
 
